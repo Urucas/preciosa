e50a5925c2695bbd2ce0ee107e0e798f5121c2ee
Martín Gaitán <mgaitan@machinalis.com>
2013-06-11 20:31:38 -0300

    importador sucursales la anonima

--- tools/importador_sucursales.py
+++ tools/importador_sucursales.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import re
 from pyquery import PyQuery
 from preciosa.precios.models import Cadena, Ciudad, Sucursal
 
@@ -6,6 +7,12 @@ from preciosa.precios.models import Cadena, Ciudad, Sucursal
 class smart_dict(dict):
     def __missing__(self, key):
         return key
+
+
+def replace_all(text, dic):
+    for i, j in dic.iteritems():
+        text = text.replace(i, j)
+    return text
 
 
 def import_coto():
@@ -72,5 +79,43 @@ def import_coto():
                                horarios=horarios, telefono=telefono, cadena=COTO)
 
 
+def importador_laanonima():
+    dir_pattern = re.compile(r'(?P<suc>Suc.*\: )?(?P<dir>.*),?.*\((?P<cp>\d+)\), (?P<ciu>.*), (?P<prov>.*)$')
+    URL_BASE = 'http://www.laanonima.com.ar/sucursales/sucursal.php?id='
+    LA_ANONIMA = Cadena.objects.get(nombre=u"La Anónima")
+
+    for i in range(1, 159):
+        url = URL_BASE + str(i)
+        pq = PyQuery(url)
+        nombre = pq('td.titulos').eq(0).text().strip()
+        if not nombre:
+            continue
+        descripcion = pq('td.descipciones').eq(1).text()
+        if 'Superquik' in descripcion or 'Quick' in descripcion:
+            continue
+        try:
+            horarios = re.search(r'atenci\xf3n: (.*) \xc1rea', descripcion).groups()[0]
+        except (AttributeError, IndexError):
+            horarios = ''
+        direccion = pq('td.descipciones').eq(2).text()
+        try:
+            _, direccion, cp, ciudad, provincia = dir_pattern.match(direccion).groups()
+        except (TypeError, AttributeError):
+            ciudad, _ = Ciudad.objects.get_or_create(nombre='Rio Gallegos',
+                                                  provincia='Santa Cruz')
+            cp = None
+            print 'Fallo: ', url
+        else:
+            direccion = direccion[:-2]
+            provincia = replace_all(provincia, dict(zip(u'áéíóú', 'aeiou')))
+            ciudad, _ = Ciudad.objects.get_or_create(nombre=ciudad, provincia=provincia)
+
+        telefono = pq('td.descipciones').eq(3).text()
+        print Sucursal.objects.create(nombre=nombre, ciudad=ciudad, direccion=direccion,
+                               horarios=horarios, telefono=telefono, cadena=LA_ANONIMA,
+                               cp=cp)
+
+
+
 if __name__ == '__main__':
-    import_coto()
+    importador_laanonima()
